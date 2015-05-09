1a39bbc3da39ea44e6a978ec403f3058d0681046
Joshua Newman <josh@coursera.org>
2015-05-09 03:08:59 -0700

    Look up annotated object symbol in enclosing owner.

--- macros/src/main/scala/macros/SimpleCompanionImpl.scala
+++ macros/src/main/scala/macros/SimpleCompanionImpl.scala
@@ -7,8 +7,16 @@ object SimpleCompanionImpl {
   def impl(c: whitebox.Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
     val result = {
+      val owner = c.internal.enclosingOwner
+      val ownerScope = owner.info.decls
       annottees.map(_.tree).toList match {
         case q"object $name extends ..$parents { ..$body }" :: Nil =>
+          val companionObjectSymbol = ownerScope.find(_.name == name).getOrElse {
+            c.abort(c.enclosingPosition, s"Name $name not found in scope $owner")
+          }
+          val traitSymbol = companionObjectSymbol.companion
+          println("trait symbol: " + traitSymbol)
+          println("object symbol: " + companionObjectSymbol)
           q"""
             object $name extends ..$parents {
               ..$body
