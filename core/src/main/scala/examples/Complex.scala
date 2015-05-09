2d1dfa818d3385bb0e90afe53d99fa697d5c0add
Joshua Newman <josh@coursera.org>
2015-05-09 03:49:39 -0700

    Identify record field methods.

--- core/src/main/scala/examples/Complex.scala
+++ core/src/main/scala/examples/Complex.scala
@@ -5,6 +5,9 @@ import macros.SimpleCompanion
 trait Complex {
   def real: Double
   def imag: Double
+
+  def gt(other: Complex): Boolean = abs > other.abs
+  def abs: Double = Math.sqrt(real * real + imag * imag)
 }
 
 @SimpleCompanion
--- macros/src/main/scala/macros/SimpleCompanionImpl.scala
+++ macros/src/main/scala/macros/SimpleCompanionImpl.scala
@@ -6,27 +6,46 @@ object SimpleCompanionImpl {
 
   def impl(c: whitebox.Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
-    val result = {
-      val owner = c.internal.enclosingOwner
-      val ownerScope = owner.info.decls
-      annottees.map(_.tree).toList match {
-        case q"object $name extends ..$parents { ..$body }" :: Nil =>
-          val companionObjectSymbol = ownerScope.find(_.name == name).getOrElse {
-            c.abort(c.enclosingPosition, s"Name $name not found in scope $owner")
-          }
-          val traitSymbol = companionObjectSymbol.companion
-          println("trait symbol: " + traitSymbol)
-          println("object symbol: " + companionObjectSymbol)
-          q"""
-            object $name extends ..$parents {
-              ..$body
-              override def toString: ${typeOf[String]} = {
-                s"custom toString for " + ${Constant(name.decodedName.toString)}
-              }
-            }
-          """
-      }
+
+    val owner = c.internal.enclosingOwner
+    val ownerScope = owner.info.decls
+
+    val recordCompanionTree = annottees.map(_.tree).toList match {
+      case head :: Nil => head
+      case _ => c.abort(c.enclosingPosition, "Expected a single annotated tree")
     }
+
+    val recordCompanionSymbol = recordCompanionTree match {
+      case q"object $name" =>
+        ownerScope.find(_.name == name).getOrElse {
+          c.abort(c.enclosingPosition, s"Name $name not found in scope $owner")
+        }
+    }
+
+    val recordSymbol = recordCompanionSymbol.companion
+
+    def isRecordField(method: MethodSymbol): Boolean = {
+      method.isAbstract && method.isPublic && method.paramLists.isEmpty
+    }
+
+    val fieldMethods = recordSymbol.info.decls.collect {
+      case method: MethodSymbol if isRecordField(method) => method
+    }
+
+    println("field methods: " + fieldMethods)
+
+    val result = annottees.map(_.tree).toList match {
+      case q"object $name extends ..$parents { ..$body }" :: Nil =>
+        q"""
+          object $name extends ..$parents {
+            ..$body
+            override def toString: ${typeOf[String]} = {
+              s"custom toString for " + ${Constant(name.decodedName.toString)}
+            }
+          }
+        """
+    }
+
     c.Expr[Any](result)
   }
 
