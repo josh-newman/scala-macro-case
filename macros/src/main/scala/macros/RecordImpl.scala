f9e5ecbf8e79b587e5fd82477c5de4d59dbf94e6
Joshua Newman <josh@coursera.org>
2015-05-09 12:15:07 -0700

    Fixed `@Record` implementation that handles the companion.

--- macros/src/main/scala/macros/RecordImpl.scala
+++ macros/src/main/scala/macros/RecordImpl.scala
@@ -7,30 +7,16 @@ private[macros] object RecordImpl {
   def impl(c: whitebox.Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
 
-    val owner = c.internal.enclosingOwner
-    val ownerScope = owner.info.decls
-
     val (recordTree, recordCompanionTree) = annottees.map(_.tree).toList match {
       case record :: Nil => (record, q"")
       case record :: recordCompanion :: Nil => (record, recordCompanion)
       case _ => c.abort(c.enclosingPosition, s"Expected an annotated `trait` and its companion")
     }
 
-    val recordSymbol = recordTree match {
-      case q"$mods trait $tpname[..$tparams] extends { ..$earlydefns } with ..$parents { $self => ..$stats }" =>
-        ownerScope.find(_.name == tpname).getOrElse {
-          c.abort(c.enclosingPosition, s"Trait $tpname not found in scope $owner")
-        }
-    }
-
     // TODO: Dedupe with `RecordCompanionImpl`.
     def isRecordField(method: MethodSymbol): Boolean = {
       method.isAbstract && method.isPublic && method.paramLists.isEmpty
     }
-
-    val fieldMethods = recordSymbol.info.decls.collect {
-      case method: MethodSymbol if isRecordField(method) => method
-    }.toList
 
     val result = recordTree match {
       // TODO: Add other quasiquotes `trait` fields.
@@ -41,13 +27,18 @@ private[macros] object RecordImpl {
           ..$stats
         }""" =>
 
+        val fieldNames = stats.collect {
+          case q"def $tname: $tpt" => tname
+        }
+
         val generatedToString = {
-          val fieldsList = fieldMethods.map { method =>
-            q"""${Constant(method.name.toString)} + "=" + ${method.name}"""
+          val fieldsList = fieldNames.map { fieldName =>
+            q"""${Constant(fieldName.toString)} + "=" + $fieldName"""
           }
+          val listApply = q"_root_.scala.collection.immutable.List.apply"
           q"""
             override def toString(): ${typeOf[String]} = {
-              val fieldValues = ${typeOf[List[String]]}(..$fieldsList).mkString(", ")
+              val fieldValues = $listApply[${typeOf[String]}](..$fieldsList).mkString(", ")
               ${Constant(tpname.toString)} + "[" + fieldValues + "]"
             }
           """
@@ -58,6 +49,7 @@ private[macros] object RecordImpl {
             extends { ..$earlydefns }
             with ..$parents { $self =>
             ..$stats
+            $generatedToString
           }
 
           $recordCompanionTree
